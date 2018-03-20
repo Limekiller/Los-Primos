446df4154eccfa0e3de6274354848e8b2d452320
Limekiller <bryoder@outlook.com>
2018-03-19 23:13:55 -0400

    Menu

---
+++ menu.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="./style.css" />
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="./scripts/header.js"></script>
+        <script src="./scripts/menu.js"></script>
+        <title>
+            Los Primos Mexican Grill | Menu
+        </title>
+    </head>
+    <body>
+        <div class="m_wrapper">
+            <div class="header">
+		<a href="/">
+                <div class="logo">
+                    <section class="blocker"></section>
+                    <img src="./images/chili.svg"/>
+                    <div>
+                        <h1>LOS PRIMOS</h1>
+                        <h5>MEXICAN GRILL</h5>
+                    </div>
+                </div>
+		</a>
+                <nav class="menu">
+                    <ul>
+                        <li><a href="./menu.php">Menu</a></li>
+                        <li><a href="./about.html">About</a></li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="r_menu">
+		<h1>Menu</h1>
+		<div class="m_chooser">
+			<div class="m_buttons" >
+				<h2 id="m1" class="m_buttons_uns m_buttons_select">Los Primos Restaurant & Catering</h2>
+				<h2 id="m2" class="m_buttons_uns">Los Primos Mexican Grill</h2>
+			</div>
+			<div id="rm1" class="fmenu menu_select">
+				<h2 style="margin-top:0px;">Breakfast</h2>
+				<p style="margin-bottom:15px;">Served with rice and beans</p>
+				<hr /><br />
+				<h3>Huevos Rancheros</h3>
+				<p>Eggs and a chef's special sauce</p>
+				<h3>Huevos a La Mexicana</h3>
+				<p>Mexican style eggs with tomatoes and onions</p>
+				<h3>Torta de Huevo Espanola</h3>
+				<p>Spanish egg omelette prepared with potatoes, eggs, red-green peppers & salsa</p>
+				<h3>Huevos con Tocino</h3>
+				<p>Eggs and Bacon</p>
+				<h3>Huevos en Salsa Verde</h3>
+				<p>Eggs in green salsa</p>
+				<h3>Omelette</h3>
+				<p>With mushrooms, onions, cheese & chile or choose your style</p>
+				<h3>Huevos con Chorizo</h3>
+				<p>Eggs with Mexican Sausage</p>
+				<h3>Huevos Divorciados</h3>
+				<p>Divorced eggs</p>
+				<h3>Chilaquiles (Verdes o Rojos)</h3>
+				<p>Chilaquiles in Green or Red Salsa</p>
+				<h2>Tacos, Burritos, Tortas & Quesadillas</h2>
+				<br /><hr /><br />
+				<h3>Tacos</h3>
+				<p>Your choice of meat with onions and cilantro</p>
+				<h3>Tortas</h3>
+				<p>Mexican sandwich with tomatoes, lettuce, cheese, sour cream, avocado, jalapenos, and your choice of meat</p>
+				<h3>Steak Fry Burrito</h3>
+				<p>With french fries, cheddar cheese, steak and mozzarella cheese</p>
+				<h3>Supreme Burrito</h3>
+				<p>With your choice of meat and vegetables</p>
+				<h3>Hot Crazy Burrito</h3>
+				<p>With ground beef or chicken, beans, sour cream and hot sauce</p>
+				<h3>Quesadillas</h3>
+				<p>With cheese and meat or only cheese. Pick your choice of meat</p>
+				<h3>Supreme</h3>
+				<p>With cheese and meat, tomatoes, lettuce beans, avocado, jalapenos and sour cream</p>
+				
+			</div>
+			<div id="rm2" class="fmenu ">
+				<h2>Burrito</h2>
+				<br /><hr /><br />
+				<h2>Burrito Bowl</h2>
+				<br /><hr /><br />
+				<h2>Quesadilla</h2>
+				<br /><hr /><br />
+				<h2>Tacos</h2>
+				<br /><hr /><br />
+			</div>
+		</div>
+            </div>
+	    <div style="height:200px;"></div>
+        </div>
+        <div class="footer">
+                <div class="flogo">
+                    <img src="./images/chili.svg"/>
+                    <div style="width:100%;">
+                        <h1>LOS PRIMOS</h1>
+                        <h5>MEXICAN GRILL</h5>
+                    </div>
+                </div>
+		<div class="finfo">Created by Bryce Yoder, 2018.</div>
+        </div>
+    </body>
+</html>
